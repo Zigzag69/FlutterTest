db9955d34bdc1b6cd4c22de7b6743dcaf0a3ed00
Ivan Gevelev <ivan.gevelev@pettersonapps.com>
2019-10-30 17:10:00 +0200

    removed appBar, some changes

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test_app/ui/sign_in/sign_in_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Test App'),
+      debugShowCheckedModeBanner: false,
+      home: SignInPage(),
     );
   }
 }
--- flutter_test_app/lib/ui/sign_in/sign_in_page.dart
+++ flutter_test_app/lib/ui/sign_in/sign_in_page.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class SignInPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SignInPageState createState() => _SignInPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
         body: Center(
           child: Text(
             "Flutter Test App",
