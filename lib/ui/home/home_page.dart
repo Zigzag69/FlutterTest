9f9b9221bb753fc7df68da314e7ba5d3f837e63a
Ivan Gevelev <ivan.gevelev@pettersonapps.com>
2019-11-08 17:42:25 +0200

    added item_details files, added start logic firebase database

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_test_app/common/consts/keys.dart';
@@ -7,6 +8,7 @@ import 'package:flutter_test_app/redux/home/home_actions.dart';
 import 'package:flutter_test_app/ui/home/home_vm.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,7 +17,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List _skills = new List();
-  List skillsData = ["Sketch", "Graphic design", "Web design", "Jira"];
+  List skillsData = [
+    "Name",
+    "Surname",
+    "Years",
+  ];
   GlobalKey<ScaffoldState> _scaffoldKey;
 
   @override
@@ -53,6 +59,84 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _listView(context, snapshot) {
+    return ListView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: snapshot.data.documents.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Container(
+                      height: 60,
+                      width: (MediaQuery.of(context).size.width - 116) / 2 - 5,
+                      color: Color(0xFF31373c),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Text(
+                            _skills[index],
+                            style: TextStyle(
+                              color: Color(0xFFfffff8),
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      print("tap item 1");
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: GestureDetector(
+                      child: Container(
+                        height: 60,
+                        width:
+                            (MediaQuery.of(context).size.width - 116) / 2 - 5,
+                        color: Color(0xFF31373c),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 12),
+                            child: Text(
+//                              snapshot.data.documents[index],
+                            "2",
+                              style: TextStyle(
+                                color: Color(0xFFfffff8),
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        print("tap item 2");
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/images/ic_trash_white.svg',
+                        width: 44.0,
+                        height: 44.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -85,98 +169,40 @@ class _HomePageState extends State<HomePage> {
                         constraints:
                             BoxConstraints(minHeight: constraints.maxHeight),
                         child: Padding(
-                          padding: EdgeInsets.fromLTRB(24, 134, 24, 0),
+                          padding: EdgeInsets.fromLTRB(24, 50, 24, 20),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.stretch,
                             children: <Widget>[
-                              Padding(
-                                  padding: EdgeInsets.only(top: 0),
-                                  child: Align(
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      "Home Page",
-                                      style: TextStyle(
-                                          fontSize: 40,
-                                          color: Color(0xff5eab9f),
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  )),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 20),
-                                child: ListView.builder(
-                                    physics:
-                                        const NeverScrollableScrollPhysics(),
-                                    scrollDirection: Axis.vertical,
-                                    shrinkWrap: true,
-                                    itemCount: _skills.length,
-                                    itemBuilder:
-                                        (BuildContext ctxt, int index) {
-                                      return new Padding(
-                                          padding:
-                                              const EdgeInsets.only(top: 10),
-                                          child: Row(
-                                            children: <Widget>[
-                                              Container(
-                                                height: 60,
-                                                color: Color(0xFF31373c),
-                                                child: Align(
-                                                  alignment:
-                                                      Alignment.centerLeft,
-                                                  child: Padding(
-                                                    padding:
-                                                        const EdgeInsets.only(
-                                                            left: 12),
-                                                    child: Text(
-                                                      _skills[index],
-                                                      style: TextStyle(
-                                                        color:
-                                                            Color(0xFFfffff8),
-                                                        fontSize: 16,
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                              Padding(
-                                                padding: const EdgeInsets.only(
-                                                    left: 0),
-                                                child: IconButton(
-                                                  icon: SvgPicture.asset(
-                                                    'assets/images/ic_trash_white.svg',
-                                                    width: 44.0,
-                                                    height: 44.0,
-                                                  ),
-                                                  onPressed: () {},
-                                                ),
-                                              ),
-                                            ],
-                                          ));
-                                    }),
+                              Center(
+                                child: Text(
+                                  "Home Page",
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      color: Color(0xff5eab9f),
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ),
                               Padding(
-                                  padding: const EdgeInsets.only(top: 24),
-                                  child: SizedBox(
-                                    height: 44,
-                                    child: RaisedButton(
-                                      color: Color(0xffe1594b),
-                                      elevation: 0,
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              new BorderRadius.circular(22.0),
-                                          side:
-                                              BorderSide(color: Colors.white)),
-                                      child: Text(
-                                        'Log Out',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.white,
+                                padding: const EdgeInsets.only(top: 20),
+                                child: StreamBuilder(
+                                  stream: Firestore.instance
+                                      .collection('testItems')
+                                      .snapshots(),
+                                  builder: (context, snapshot) {
+                                    if (!snapshot.hasData)
+                                      return const Center(
+                                        child: Text(
+                                          "Loading...",
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              color: Color(0xFFfffff8),
+                                              fontWeight: FontWeight.bold),
                                         ),
-                                      ),
-                                      onPressed: () {
-                                        _logOut();
-                                      },
-                                    ),
-                                  )),
+                                      );
+                                    return _listView(context, snapshot);
+                                  },
+                                ),
+                              ),
                             ],
                           ),
                         ),
@@ -187,6 +213,30 @@ class _HomePageState extends State<HomePage> {
               );
             },
           ),
+          bottomNavigationBar: Container(
+              height: 84,
+              color: Color(0xFF2a3035),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 10, bottom: 30, right: 24, left: 24),
+                child: RaisedButton(
+                  color: Color(0xffe1594b),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(22.0),
+                      side: BorderSide(color: Colors.white)),
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    _logOut();
+                  },
+                ),
+              )),
         ),
       ],
     );
